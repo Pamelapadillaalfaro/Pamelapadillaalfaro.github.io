abb959c517222c42725466aa402b629e0f8b805d
Pamelapa2002 <101819900+Pamelapadillaalfaro@users.noreply.github.com>
2025-06-25 15:56:04 -0600

    Add files via upload

---
+++ Proyectos/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Esta página muestra información sobre los jardínes verticales"/>
+    <title>Proyectos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul class=main-nav>
+            <li class=main-nav__item><a href="http://localhost/JardinVertical/">Inicio</a></li>
+            <li class=main-nav__item><a href="http://localhost/JardinVertical/Videos">Videos</a></li>
+            <li class=main-nav__item><a href="">Proyectos</a></li>
+        </ul>
+    </nav>
+    <h1>Ideas para proyectos</h1>
+    <h2>Robots en la agricultura</h2>
+    <p>Robots para quitar malas hierbas</p>
+    <figure><img src="./RobotMalasHierbas.webp" alt="" id="flowers"></figure> 
+    <a href="https://www.agrohuerto.com/robots-agricolas-para-huertos-pequenos-y-grandes/">More info</a>
+    <h2>Creación de un invernadero</h2>
+    <p>more info https://youtu.be/UqpncC5A7cw
+    </p>
+    <ul>
+        <li>Partes del proyecto</li>
+        <ol><li>Pantalla para ver los datos de los sensores</li>
+            <li>Colocacion de sensores</li>
+            <ol><li>Sensor de temperatura y humedad</li>
+                <li>Sensor de luz</li>
+                <li>Higrómetro, sensor de humedad del suelo</li>
+                <p>Dato interesante existen sensores crepusculares que pueden prender o apagar un sistema dependiendo de la luz</p>
+            </ol>
+            <li>Diseño de la estructura del invernadero</li>
+            <li>Sistema de riego, NOTA: En un video se ocupan bombas</li>
+            <p>Nota: Aunque ocupar la orange pi puede ser un poco enrevesado, sigue siendo una muy buena opcion debido a la memoria que tiene</p>
+        </ol>
+    </ul>
+</body>
+</html>
